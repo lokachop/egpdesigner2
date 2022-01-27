77ea6608f19b08c682dde3b6accc00e7cf5edfdd
lokachop <lokachop@gmail.com>
2022-01-27 19:58:46 +0100

    throw helpful message if user dont have love2d

--- run.bat
+++ run.bat
@@ -1,4 +1,9 @@
 ::most basic ass bat file to launch the program, by lokachop
 @echo off
 echo egpdesigner2, by lokachop
-love %CD% --console
+love %CD% --console && (
+    echo ran properly!
+) || (
+    echo love2d doesnt seem to be installed, please refer to the instructions in https://github.com/lokachop/egpdesigner2
+    pause
+)
